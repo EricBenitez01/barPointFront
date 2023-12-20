c076943bfa0ab5f1d118cecd9fde3e0a8f38e2de
toledoardev <alejandrotoledodev@gmail.com>
2023-12-20 19:24:39 -0300

    Se quita hardcodeo de imégenes en  promotions

--- src/app/components/business/promotions/promotions.component.ts
+++ src/app/components/business/promotions/promotions.component.ts
@@ -29,5 +29,14 @@ export class PromotionsComponent implements OnInit {
         this.benefitsService.benefitsForaBusiness(this.businessId).subscribe((result) => {
             this.benefits = result.data;
         })
-    }
+    };
+
+    getImagenUrl(imagenNombre: string): string {
+        if (imagenNombre) {
+            return `http://localhost:3001/images/${imagenNombre}`;
+        }
+        else {
+            return "../../../assets/defaultImage.png";
+        }
+    };
 }
